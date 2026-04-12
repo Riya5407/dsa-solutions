a1830b59c89ccd33934f19e5fb1ee5ade7544fba
Riya5407 <riyasp050407@gmail.com>
2026-04-12 14:57:52 +0530

    feat(arrays): add solutions for 4Sum, Merge Intervals, and Rotate Array (Day 3)

---
+++ arrays/18_4Sum.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        ans = []
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]: continue
+            for j in range(i + 1, n):
+                if j > i + 1 and nums[j] == nums[j-1]: continue
+                
+                # 2-pointer
+                k = j + 1
+                l = n - 1
+                while k < l:
+                    _sum = nums[i] + nums[j] + nums[k] + nums[l]
+                    if _sum == target:
+                        ans.append([nums[i], nums[j], nums[k], nums[l]])
+                        k += 1
+                        l -= 1
+                        while k < l and nums[k] == nums[k-1]: k += 1
+                        while k < l and nums[l] == nums[l+1]: l -= 1
+                    elif _sum < target:
+                        k += 1
+                    else:
+                        l -= 1
+        return ans
