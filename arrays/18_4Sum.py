7ea332f533e3f24fe483a519478ad92a2eba9c21
Riya5407 <riyasp050407@gmail.com>
2026-04-12 15:09:40 +0530

    refactor(arrays): replace auto-generated code with user's actual LeetCode submissions for Day 3

--- arrays/18_4Sum.py
+++ arrays/18_4Sum.py
@@ -1,28 +1,31 @@
-from typing import List
-
-class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
+class Solution(object):
+    def fourSum(self, nums, target):
         n = len(nums)
+        nums.sort()
         ans = []
         for i in range(n):
-            if i > 0 and nums[i] == nums[i-1]: continue
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             for j in range(i + 1, n):
-                if j > i + 1 and nums[j] == nums[j-1]: continue
-                
-                # 2-pointer
-                k = j + 1
-                l = n - 1
-                while k < l:
-                    _sum = nums[i] + nums[j] + nums[k] + nums[l]
-                    if _sum == target:
-                        ans.append([nums[i], nums[j], nums[k], nums[l]])
-                        k += 1
-                        l -= 1
-                        while k < l and nums[k] == nums[k-1]: k += 1
-                        while k < l and nums[l] == nums[l+1]: l -= 1
-                    elif _sum < target:
-                        k += 1
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left, right = j + 1, n - 1
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if total == target:
+                        ans.append([nums[i], nums[j], nums[left], nums[right]])
+
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+
+                        left += 1
+                        right -= 1
+                    elif total < target:
+                        left += 1
                     else:
-                        l -= 1
+                        right -= 1
+
         return ans
--- arrays/56_Merge_Intervals.py
+++ arrays/56_Merge_Intervals.py
@@ -1,13 +1,10 @@
-from typing import List
-
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals: return []
-        intervals.sort(key=lambda x: x[0])
-        merged = [intervals[0]]
-        for i in range(1, len(intervals)):
-            if intervals[i][0] <= merged[-1][1]:
-                merged[-1][1] = max(merged[-1][1], intervals[i][1])
+class Solution(object):
+    def merge(self, intervals):
+        intervals.sort()
+        ans = []
+        for interval in intervals:
+            if not ans or ans[-1][1] < interval[0]:
+                ans.append(interval)
             else:
-                merged.append(intervals[i])
-        return merged
+                ans[-1][1] = max(ans[-1][1], interval[1])
+        return ans
